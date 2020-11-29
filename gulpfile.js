06f0f6a940a6bb238887f0b4216856bb508448a4
長谷川 裕大 <gssbtsm999@gmail.com>
2020-11-30 02:14:39 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const CSS_PATH = './public/css/'
 
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
 
 sass.compiler = require('node-sass')
 
@@ -12,6 +14,7 @@ gulp.task('sass', () => {
   return gulp
     .src(SASS_PATH)
     .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
     .pipe(gulp.dest(CSS_PATH))
 })
 
